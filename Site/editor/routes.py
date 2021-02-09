3d6324581ef8d71900f2e360141dfcbe34696c78
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-02-09 14:59:23 +0100

    Add files via upload

--- Site/editor/routes.py
+++ Site/editor/routes.py
@@ -6,33 +6,226 @@ from Site import db, app # importing database and app configuration from folder 
 from flask_login import current_user, login_required, login_user, logout_user # using flask login
 from werkzeug.security import check_password_hash, generate_password_hash # using flask security
 import datetime
+
+
 # registering blueprint 
 edit = Blueprint('edit', __name__)
 
+import json
+import uuid
+import os
+
+from flask import Blueprint, render_template, request, jsonify
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
+from wtforms import FileField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+
+class InFormTeam(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+
+class Tips(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class HighGoalStats(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class Bothteamsscore(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class HighConcedingRate(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class BetAgainst(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+
+class sureOddsForm(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+
+class championshipDrawForm(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class RandomPickForm(FlaskForm):
+    content = StringField(validators=[DataRequired()])
+    country = StringField(validators=[DataRequired()])
+    league = StringField(validators=[DataRequired()])
+    submit = SubmitField('Post')
 
 
 @edit.route('/')
 def home():
 
-    pick = RandomPrediction.query.filter(RandomPrediction.dateUploaded >= (datetime.datetime.now() - datetime.timedelta(days=1))).all()
+    pick = RandomPrediction.query.all()
     prediction = RandomPrediction.query.filter(RandomPrediction.dateUploaded >= (datetime.datetime.now() - datetime.timedelta(days=7))).all()
 
     return render_template('index.html', pick=pick)
 
 
-@edit.route('/post', methods=['GET','POST'])
-def post():
+@edit.route('/1', methods=['GET','POST'])
+def a():
+    form = InFormTeam()
+    if form.validate_on_submit():
+        inform= InForm()
+        inform.content = form.content.data
+        inform.country = form.country.data
+        inform.league = form.league.data
+        db.session.add(inform)
+        db.session.commit()
+        return redirect(url_for("edit.a"))
+    
+    return render_template('post.html', form=form, legend='In Form Teams')
 
 
-    return render_template('post.html')
+@edit.route('/2', methods=['GET','POST'])
+def b():
+    form= Tips()
+    if form.validate_on_submit():
+        bettips= BettingTips()
+        bettips.content= form.content.data
+        bettips.country = form.country.data
+        bettips.league = form.league.data
+        db.session.add(bettips)
+        db.session.commit()
+        return redirect(url_for("edit.b"))
+    return render_template('post.html', form=form, legend='Betting Tips')
 
-@edit.route('/update', methods=['GET','POST'])
-def update():
 
 
-    return render_template('post.html')
+@edit.route('/3', methods=['GET','POST'])
+def c():
+    form = HighGoalStats()
+    if form.validate_on_submit():
+        highscoring= HighScoring()
+        highscoring.content= form.content.data
+        highscoring.country = form.country.data
+        highscoring.league = form.league.data
+        db.session.add(highscoring)
+        db.session.commit()
+        return redirect(url_for("edit.c"))
+    return render_template('post.html', form=form, legend='High scoring stats')
 
 
+@edit.route('/4', methods=['GET','POST'])
+def d():
+    form= Bothteamsscore()
+    if form.validate_on_submit():
+        bts= Bts()
+        bts.content = form.content.data
+        bts.country = form.country.data
+        bts.league = form.league.data
+        db.session.add(bts)
+        db.session.commit()
+        return redirect(url_for("edit.d"))
+    return render_template('post.html', form=form, legend='Both teams to score')
+
+
+
+@edit.route('/5', methods=['GET','POST'])
+def e():
+    form = HighConcedingRate()
+    if form.validate_on_submit():
+        highconcede = HighConceding()
+        highconcede.content = form.content.data
+        highconcede.country = form.country.data
+        highconcede.league = form.league.data
+        db.session.add(highconcede)
+        db.session.commit()
+        return redirect(url_for("edit.e"))
+    return render_template('post.html', form=form, legend='High concedeing rate')
+
+
+
+
+@edit.route('/6', methods=['GET','POST'])
+def f():
+    
+    form= BetAgainst()
+    if form.validate_on_submit():
+        betagainst= BetAgainst()
+        betagainst.content = form.content.data
+        betagainst.country = form.country.data
+        betagainst.league = form.league.data
+        db.session.add(betagainst)
+        db.session.commit()
+        return redirect(url_for("edit.f"))
+    return render_template('post.html', form=form, legend='Teams to bet against')
+
+
+@edit.route('/7', methods=['GET','POST'])
+def g():
+    form= sureOddsForm()
+    if form.validate_on_submit():
+        sureodd = SureOdds()
+        sureodd.content= form.content.data
+        sureodd.country = form.country.data
+        sureodd.league = form.league.data
+        db.session.add(sureodd)
+        db.session.commit()
+        return redirect(url_for("edit.g"))
+
+
+    return render_template('post.html', form=form, legend='Sure odds')
+
+@edit.route('/8', methods=['GET','POST'])
+def h():
+    form= championshipDrawForm()
+    if form.validate_on_submit():
+        
+        return redirect(url_for("edit.h"))
+
+    return render_template('post.html', form=form, legend='Championship draws')
+
+@edit.route('/9', methods=['GET','POST'])
+def i():
+    form= RandomPickForm()
+    if form.validate_on_submit():
+        rand= RandomPrediction()
+        rand.content= form.content.data
+        rand.country = form.country.data
+        rand.league = form.league.data
+        db.session.add(rand)
+        db.session.commit()
+        return redirect(url_for("edit.i"))
+
+    return render_template('post.html', form=form, legend='Random picks')
 
 @edit.route('/contact')
 def contact():
@@ -42,47 +235,47 @@ def contact():
 
 @edit.route('/in-form-teams', methods=['GET','POST'])
 def mostInFormTeams():
-
-    return render_template('informteam.html')
+    inform = InForm.query.all()
+    return render_template('informteam.html', inform=inform)
 
 
 
 @edit.route('/betting-tips', methods=['GET','POST'])
 def bettingTips():
-
-   return render_template('betting-tips.html')
+    bet=BettingTips.query.all()
+    return render_template('betting-tips.html',  bet=bet)
 
 @edit.route('/teams-with-high-scoring-stats', methods=['GET','POST'])
 def teamsWithHighGoalStats():
-
-
-    return render_template('high-scoring-stats.html')
+    highscore= HighScoring.query.all()
+    return render_template('high-scoring-stats.html',  highscore=highscore)
 
 @edit.route('/both-team-score-tips', methods=['GET','POST'])
 def bothTeamScoreTips():
+    bts = Bts.query.all()
 
-    return render_template('both-team-score.html')
+    return render_template('both-team-score.html',  bts=bts)
 
 
 
 @edit.route('/teams-with-high-conceding-rate', methods=['GET','POST'])
 def teamsWithHighConcedingRate():
-
-    return render_template('high-conceding.html')
+    highconcede =HighConceding.query.all()
+    return render_template('high-conceding.html', highconcede=highconcede)
 
 
 @edit.route('/teams-to-bet-against', methods=['GET','POST'])
 def teamsToBetAgainst():
-
-    return render_template('bet-against.html')
+    betagainst = BetAgainst.query.all()
+    return render_template('bet-against.html', betagainst=betagainst)
 
 @edit.route('/sure-odds', methods=['GET','POST'])
 def sureOdds():
-
-    return render_template('sure-odds.html')
+    sureodds= SureOdds.query.all()
+    return render_template('sure-odds.html', sureodds=sureodds)
 
 
 @edit.route('/championship-draws', methods=['GET','POST'])
 def championshipDraw():
-
-    return render_template('championship-draws.html')
+    championship= Championship.query.all()
+    return render_template('championship-draws.html', championship=championship)
