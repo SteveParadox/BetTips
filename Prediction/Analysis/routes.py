e3e841369c7220d55f4589f4858301a708cd6df3
SteveParadox <fordstphn@gmail.com>
2023-08-30 00:28:15 +0100

    Sapa

--- Prediction/Analysis/routes.py
+++ Prediction/Analysis/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for, Blueprint, abort, jsonify # importing libraries from framework
 import uuid 
-from ..models import  Teams, InForm, Bts, BettingTips, HighScoring, HighConceding
+from ..models import  Teams, InForm, Bts, BettingTips, HighScoring, HighConceding, TeamsSchema
 from Prediction import db, app 
 import datetime
 from Prediction.Tasks.preprocessing_task import commit_teams
@@ -16,3 +16,10 @@ def add_teams():
 @analysis.route('/api')
 def home():
     return jsonify({'message': 'Heheheheh'})
+
+@analysis.route('/api/all_teams')
+def teams():
+    results = Teams.query.all()
+    teams_schema = TeamsSchema(many=True)
+    res = teams_schema.dump(results)
+    return jsonify(res)
