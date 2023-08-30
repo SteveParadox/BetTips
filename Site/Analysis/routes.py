64c5678ffacfa5c9be74e82253983d57ec4481c5
SteveParadox <fordstphn@gmail.com>
2023-08-30 02:30:37 +0100

    Api

--- Site/Analysis/routes.py
+++ Site/Analysis/routes.py
@@ -85,7 +85,6 @@ def search():
     results = Teams.query.filter(Teams.name.ilike(f'%{data}%')).all()
     teams_schema = TeamsSchema(many=True)
     res = teams_schema.dump(results)
-    print(res.encode('utf-8'))
     return jsonify(res)
 
 
